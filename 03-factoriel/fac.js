08dece2bb1f388cc50f10acec373c613d260acda
mohammad ghahri <m.ghahri2025@gmail.com>
2024-09-13 10:51:05 +0330

    sdkfmskdf

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,25 @@
 // write your func here
+
+// function fac(num) {
+//   let sum = 1;
+//   for (let index = 1; index <= num; index++) {
+//     sum *= index;
+//   }
+
+//   return sum;
+// }
+
+function fac(num) {
+  let arr = [];
+  for (let index = 0; index < num; index++) {
+    arr.push(index + 1);
+  }
+
+  return arr.reduce((acc, current) => {
+    return acc * current;
+  }, 1);
+}
+
+console.log(fac(5));
+console.log(fac(10));
+console.log(fac(20));
--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,11 @@
 // write your func here
+function findMax(arr) {
+  let max = arr[0];
+  arr.forEach((element) => {
+    if (element > max) max = element;
+  });
+
+  return max;
+}
+
+console.log(findMax([10, 15, -60, -1, 20, 8]));
