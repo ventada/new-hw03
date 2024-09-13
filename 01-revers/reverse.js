8b13794b216234a25868c62ac802673d90971c02
mohammad ghahri <m.ghahri2025@gmail.com>
2024-09-13 10:29:46 +0330

    finished ex01

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,14 @@
 // write your func here
+
+// function revers(str) {
+//   let sum = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     sum += str[i];
+//   }
+//   return sum;
+// }
+function revers(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(revers("this is a string from hw03"));
