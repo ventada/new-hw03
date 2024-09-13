4d2ccc63f8cf08959c3c315201fbb83849621866
mohammad ghahri <m.ghahri2025@gmail.com>
2024-09-13 11:01:57 +0330

    finish 7

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,14 @@
 // write your func here
+function comapareTowArr(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] !== arr2[index]) return false;
+  }
+
+  return true;
+}
+
+console.log(comapareTowArr([1, 2, 3], [1, 2, 3]));
+console.log(comapareTowArr([1, 2, 3], [1, 2, 3, 5]));
+console.log(comapareTowArr([1, 2, 3], [1, 2, "3"]));
