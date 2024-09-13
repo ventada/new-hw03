c5699ef67e8ee563aa1f72c7dbcb46964b973410
mohammad ghahri <m.ghahri2025@gmail.com>
2024-09-13 10:32:13 +0330

    ex023

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,11 @@
 // write your func here
+function revers(str) {
+  return str.split("").reverse().join("");
+}
+
+function plaindrom(str) {
+  return str === revers(str);
+}
+
+console.log(plaindrom("racecar"));
+console.log(plaindrom(";sidfjsjf"));
